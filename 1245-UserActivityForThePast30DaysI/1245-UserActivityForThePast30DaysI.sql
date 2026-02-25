ef3320593e496532341a04e36bb30d4c5436437a
Kirithish Kathiresan <kirithishkkm@gmail.com>
2026-02-25 09:09:22 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,5 @@
+-- Last updated: 2/25/2026, 9:09:21 AM
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
