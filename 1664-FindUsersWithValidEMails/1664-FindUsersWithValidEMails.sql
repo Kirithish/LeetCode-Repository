06ce65a60a61c2b332da4af8d3304aa4a4c0dc79
Kirithish Kathiresan <kirithishkkm@gmail.com>
2026-02-25 09:09:14 +0530

    Create 1664-FindUsersWithValidEMails

---
+++ 1664-FindUsersWithValidEMails/1664-FindUsersWithValidEMails.sql
@@ -0,0 +1,10 @@
+-- Last updated: 2/25/2026, 9:09:13 AM
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';
