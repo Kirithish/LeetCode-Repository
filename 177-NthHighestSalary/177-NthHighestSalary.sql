9d63ccf24e35be285aba51afbe8c0cca4a349a24
Kirithish Kathiresan <kirithishkkm@gmail.com>
2026-02-25 09:10:12 +0530

    Create 177-NthHighestSalary

---
+++ 177-NthHighestSalary/177-NthHighestSalary.sql
@@ -0,0 +1,9 @@
+-- Last updated: 2/25/2026, 9:10:12 AM
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N=N-1;
+  RETURN (
+    
+    SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET N
+  );
+END
