ef31e0d6d0d7a58b24d246340c3637daf52eaf9e
Kirithish Kathiresan <kirithishkkm@gmail.com>
2026-06-10 19:59:48 +0530

    File updated at 6/10/2026, 7:59:48 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,16 +1,20 @@
-// Last updated: 6/10/2026, 7:50:34 PM
+// Last updated: 6/10/2026, 7:59:48 PM
 1class Solution {
-2    public int[] twoSum(int[] nums, int target) {
-3        for(int i=0;i<nums.length;i++){
-4            for(int j=i+1;j<nums.length;j++){
-5                if(j!=i){
-6                    if(nums[i]+nums[j]==target){
-7                        return new int[]{i,j};
-8                    }
-9                }
-10            }
-11        }
-12        return new int[]{-1,-1};
-13        
-14    }
-15}
+2    public boolean isAnagram(String s, String t) {
+3       if(s.length()!=t.length()){
+4        return false;
+5       }
+6
+7       char[] c=s.toCharArray();
+8       char[] p=t.toCharArray();
+9
+10       Arrays.sort(c);
+11       Arrays.sort(p);
+12
+13       return new String(c).equals(new String(p));
+14
+15
+16
+17
+18    }
+19}
