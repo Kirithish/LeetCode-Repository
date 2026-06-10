56e56692da0932fc46f210db7b8ac6034cddac2d
Kirithish Kathiresan <kirithishkkm@gmail.com>
2026-06-10 20:21:29 +0530

    File updated at 6/10/2026, 8:21:30 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,12 +1,10 @@
-// Last updated: 6/10/2026, 8:16:23 PM
+// Last updated: 6/10/2026, 8:21:30 PM
 1class Solution {
 2    public boolean containsDuplicate(int[] nums) {
-3        Arrays.sort(nums);
-4        for(int i=0;i<nums.length-1;i++){
-5            if(nums[i]==nums[i+1]){
-6                return true;
-7            }
-8        }
-9        return false;
-10    }
-11}
+3        HashSet<Integer> set=new HashSet<>();
+4        for(int i=0;i<nums.length;i++){
+5            set.add(nums[i]);
+6        }
+7        return set.size()!=nums.length;
+8    }
+9}
