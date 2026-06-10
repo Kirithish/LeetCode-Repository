9ffbfe14bea0c34160a2ffd757ea3663c008d2cc
Kirithish Kathiresan <kirithishkkm@gmail.com>
2026-06-10 20:09:12 +0530

    File updated at 6/10/2026, 8:09:12 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,12 +1,23 @@
-// Last updated: 2/25/2026, 9:09:56 AM
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        char[] ch=s.toCharArray();
-        char[] ph=t.toCharArray();
-
-        Arrays.sort(ch);
-        Arrays.sort(ph);
-
-        return new String(ch).equals(new String(ph));
-    }
-}
+// Last updated: 6/10/2026, 8:09:12 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3       int[] ch=new int[26];
+4       for(int i=0;i<s.length();i++){
+5            ch[s.charAt(i) -'a']++;
+6
+7       }
+8       for(int j=0;j<t.length();j++){
+9            ch[t.charAt(j) - 'a']--;
+10       }
+11
+12       for(int i:ch){
+13            if(i!=0){
+14                return false;
+15            }
+16       }
+17       return true;
+18
+19
+20
+21    }
+22}
