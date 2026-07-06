fae993a636bf6d3f559ec219488ab0cf42990b97
Kirithish Kathiresan <kirithishkkm@gmail.com>
2026-07-06 11:53:51 +0530

    File updated at 7/6/2026, 11:53:50 AM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.java
@@ -1,17 +1,16 @@
-// Last updated: 7/6/2026, 11:52:41 AM
-1class Solution {
-2    public int findDuplicate(int[] nums) {
-3        HashSet<Integer> set=new HashSet<>();
-4        
-5        for(int i=0;i<nums.length;i++){
-6            if(set.contains(nums[i])){
-7                return nums[i];
-8            }
-9            else{
-10                set.add(nums[i]);
-11            }
-12
-13        }
-14        return 1;
-15    }
-16}
+// Last updated: 7/6/2026, 11:53:50 AM
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+         int[] freq = new int[nums.length];
+        for (int num : nums) {
+            freq[num]++;
+
+            if (freq[num] > 1) {
+                return num;
+            }
+        }
+        return -1;
+        
+    }
+}
